6598a5b3da502d7ee18c1a843c80fe6184950e99
DomMorello <zuzudnf@gmail.com>
2022-03-26 17:42:50 +0900

    build: 배포 페이지를 위한 dist 업로드

--- src/ts/utils.ts
+++
@@ -1,8 +0,0 @@
-export const $ = (selector: string, baseElement: HTMLElement | Document = document): HTMLElement =>
-  baseElement.querySelector(selector);
-export const $$ = (selector: string, baseElement: HTMLElement | Document = document): NodeList =>
-  baseElement.querySelectorAll(selector);
-
-export const getRandomNumber = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
